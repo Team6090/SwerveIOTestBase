f4d5e8a98b8a173b664f9c0da4e9950cbb1aa9fd
Jordan Bancino <jordan@bancino.net>
2021-01-24 17:36:42 -0500

    Update constants and TODO

--- src/main/java/frc/robot/Const.java
+++ src/main/java/frc/robot/Const.java
@@ -19,9 +19,9 @@ public class Const {
     }
 
     public static class PID {
-        public static final double SWERVE_MODULE_RAMP_RATE = 0;
-        public static final double SWERVE_MODULE_P         = 0.003;
-        public static final double SWERVE_MODULE_I         = 0.00000155;
+        public static final double SWERVE_MODULE_RAMP_RATE = 0.0;
+        public static final double SWERVE_MODULE_P         = 0.00007;
+        public static final double SWERVE_MODULE_I         = 0;
         public static final double SWERVE_MODULE_D         = 0;
     }
 
@@ -29,15 +29,10 @@ public class Const {
      * Encoders - Analog Ports and position offsets
      */
     public static class Encoder {
-        public static final int FRONT_RIGHT_ANALOG_ENCODER = 0; /* Module 1 */
-        public static final int FRONT_LEFT_ANALOG_ENCODER  = 1; /* Module 2 */
-        public static final int REAR_LEFT_ANALOG_ENCODER   = 2; /* Module 3 */
-        public static final int REAR_RIGHT_ANALOG_ENCODER  = 3; /* Module 4 */
-
         /* The encoder offsets tell us where zero is for each motor. */
-        public static final double FRONT_RIGHT_ENCODER_OFFSET = 146.27;
-        public static final double FRONT_LEFT_ENCODER_OFFSET  = 134.60;
-        public static final double REAR_LEFT_ENCODER_OFFSET   = 59.34;
-        public static final double REAR_RIGHT_ENCODER_OFFSET  = 267.2;
+        public static final double FRONT_RIGHT_ENCODER_OFFSET = 0;
+        public static final double FRONT_LEFT_ENCODER_OFFSET  = 0;
+        public static final double REAR_LEFT_ENCODER_OFFSET   = 0;
+        public static final double REAR_RIGHT_ENCODER_OFFSET  = 0;
     }
 }
