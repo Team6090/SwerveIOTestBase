a1813b4c4a4634b0f9c6a848b43afeaf6fcbf2e9
Jordan Bancino <jordan@bancino.net>
2021-02-13 14:30:10 -0500

    Remove Rotate command.
    It isn't used anymore, and it isn't necessary.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj.DriverStation;
-import frc.robot.commands.Rotate;
 import frc.robot.commands.LimelightAlign;
 import frc.robot.commands.LimelightRotate;
 import frc.robot.subsystems.DriveTrain;
