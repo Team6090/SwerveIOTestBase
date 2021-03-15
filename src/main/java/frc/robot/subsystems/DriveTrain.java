23ea4b3a21b9c3507f95ce18a612888484ca87c4
Jordan Bancino <jordan@bancino.net>
2021-03-15 14:32:09 -0400

    Replace deprecated methods with alternatives

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -13,7 +13,7 @@ import net.bancino.robotics.swerveio.SwerveDrive;
 import net.bancino.robotics.swerveio.module.SwerveModule;
 import net.bancino.robotics.swerveio.module.MK3SwerveModule;
 import net.bancino.robotics.swerveio.pid.PIDController;
-import net.bancino.robotics.swerveio.log.DashboardSwerveLogger;
+import net.bancino.robotics.swerveio.log.DashboardLog;
 import net.bancino.robotics.swerveio.geometry.Length;
 import net.bancino.robotics.swerveio.geometry.Length.Unit;
 import net.bancino.robotics.swerveio.geometry.ChassisDimension;
@@ -22,54 +22,60 @@ import net.bancino.robotics.swerveio.gyro.Gyro;
 /**
  * The drivetrain subsystem drives the robot!
  *
- * This subsystem consists of the following components:
- * - Swerve module (4x drive + pivot motor)
+ * This subsystem consists of the following components: - Swerve module (4x
+ * drive + pivot motor)
  *
- * This subsystem should provide an interface for the
- * following functions:
+ * This subsystem should provide an interface for the following functions:
  * 
- * - Running the drivetrain with joystick
- * - Running the drivetrain autonomously
+ * - Running the drivetrain with joystick - Running the drivetrain autonomously
  *
  * @author Jordan Bancino
  */
 public class DriveTrain {
 
   public static SwerveDrive create(Gyro gyro) {
-    return new SwerveDrive.Builder()
-      .useDefaultKinematics(new ChassisDimension(new Length(22.5, Unit.INCHES)))
-      // TODO: Add this back for Gyro tuning
-      //.setGyro(gyro)
-      //.setAnglePID(Const.PID.ANGLE_STANDING_SLOT, Const.PID.ANGLE_MOVING_SLOT, (pid) -> {
-      //  pid.setP(Const.PID.ANGLE_STANDING_SLOT, Const.PID.SWERVE_DRIVE_ANGLE_STANDING_P);
-      //  pid.setI(Const.PID.ANGLE_STANDING_SLOT, Const.PID.SWERVE_DRIVE_ANGLE_STANDING_I);
-      //  pid.setD(Const.PID.ANGLE_STANDING_SLOT, Const.PID.SWERVE_DRIVE_ANGLE_STANDING_D);
-      //
-      //  pid.setP(Const.PID.ANGLE_MOVING_SLOT, Const.PID.SWERVE_DRIVE_ANGLE_MOVING_P);
-      //  pid.setI(Const.PID.ANGLE_MOVING_SLOT, Const.PID.SWERVE_DRIVE_ANGLE_MOVING_I);
-      //  pid.setD(Const.PID.ANGLE_MOVING_SLOT, Const.PID.SWERVE_DRIVE_ANGLE_MOVING_D);
-      //  
-      //  pid.setAcceptableError(Const.PID.SWERVE_DRIVE_ACCEPTABLE_ERROR);
-      //})
-      .setRampRate(Const.PID.SWERVE_MODULE_RAMP_RATE)
-      .setModuleMap((map) -> {
-        map.put(SwerveModule.Location.FRONT_RIGHT, new MK3SwerveModule(Const.CAN.FRONT_RIGHT_DRIVE_MOTOR, Const.CAN.FRONT_RIGHT_PIVOT_MOTOR, Const.CAN.FRONT_RIGHT_ENCODER, Const.Angle.FRONT_RIGHT_OFFSET));
-        map.put(SwerveModule.Location.FRONT_LEFT, new MK3SwerveModule(Const.CAN.FRONT_LEFT_DRIVE_MOTOR, Const.CAN.FRONT_LEFT_PIVOT_MOTOR, Const.CAN.FRONT_LEFT_ENCODER, Const.Angle.FRONT_LEFT_OFFSET));
-        map.put(SwerveModule.Location.REAR_LEFT, new MK3SwerveModule(Const.CAN.REAR_LEFT_DRIVE_MOTOR, Const.CAN.REAR_LEFT_PIVOT_MOTOR, Const.CAN.REAR_LEFT_ENCODER, Const.Angle.REAR_LEFT_OFFSET));
-        map.put(SwerveModule.Location.REAR_RIGHT, new MK3SwerveModule(Const.CAN.REAR_RIGHT_DRIVE_MOTOR, Const.CAN.REAR_RIGHT_PIVOT_MOTOR, Const.CAN.REAR_RIGHT_ENCODER, Const.Angle.REAR_RIGHT_OFFSET));
-      }, (module) -> {
-        PIDController modulePid = module.getPivotPIDController();
-        //modulePid.setOutputRampRate(Const.PID.SWERVE_MODULE_RAMP_RATE);
-        modulePid.setP(Const.PID.SWERVE_MODULE_P);
-        modulePid.setI(Const.PID.SWERVE_MODULE_I);
-        modulePid.setD(Const.PID.SWERVE_MODULE_D);
-      })
-      .build((swerve) -> {
-        //swerve.setFieldCentric(false);
+    return new SwerveDrive.Builder().useDefaultKinematics(new ChassisDimension(new Length(22.5, Unit.INCHES)))
+        // TODO: Add this back for Gyro tuning
+        // .setGyro(gyro)
+        // .setAnglePID(Const.PID.ANGLE_STANDING_SLOT, Const.PID.ANGLE_MOVING_SLOT,
+        // (pid) -> {
+        // pid.setP(Const.PID.ANGLE_STANDING_SLOT,
+        // Const.PID.SWERVE_DRIVE_ANGLE_STANDING_P);
+        // pid.setI(Const.PID.ANGLE_STANDING_SLOT,
+        // Const.PID.SWERVE_DRIVE_ANGLE_STANDING_I);
+        // pid.setD(Const.PID.ANGLE_STANDING_SLOT,
+        // Const.PID.SWERVE_DRIVE_ANGLE_STANDING_D);
+        //
+        // pid.setP(Const.PID.ANGLE_MOVING_SLOT, Const.PID.SWERVE_DRIVE_ANGLE_MOVING_P);
+        // pid.setI(Const.PID.ANGLE_MOVING_SLOT, Const.PID.SWERVE_DRIVE_ANGLE_MOVING_I);
+        // pid.setD(Const.PID.ANGLE_MOVING_SLOT, Const.PID.SWERVE_DRIVE_ANGLE_MOVING_D);
+        //
+        // pid.setAcceptableError(Const.PID.SWERVE_DRIVE_ACCEPTABLE_ERROR);
+        // })
+        .setModuleMap((map) -> {
+          map.put(SwerveModule.Location.FRONT_RIGHT, new MK3SwerveModule(Const.CAN.FRONT_RIGHT_DRIVE_MOTOR,
+              Const.CAN.FRONT_RIGHT_PIVOT_MOTOR, Const.CAN.FRONT_RIGHT_ENCODER, Const.Angle.FRONT_RIGHT_OFFSET));
+          map.put(SwerveModule.Location.FRONT_LEFT, new MK3SwerveModule(Const.CAN.FRONT_LEFT_DRIVE_MOTOR,
+              Const.CAN.FRONT_LEFT_PIVOT_MOTOR, Const.CAN.FRONT_LEFT_ENCODER, Const.Angle.FRONT_LEFT_OFFSET));
+          map.put(SwerveModule.Location.REAR_LEFT, new MK3SwerveModule(Const.CAN.REAR_LEFT_DRIVE_MOTOR,
+              Const.CAN.REAR_LEFT_PIVOT_MOTOR, Const.CAN.REAR_LEFT_ENCODER, Const.Angle.REAR_LEFT_OFFSET));
+          map.put(SwerveModule.Location.REAR_RIGHT, new MK3SwerveModule(Const.CAN.REAR_RIGHT_DRIVE_MOTOR,
+              Const.CAN.REAR_RIGHT_PIVOT_MOTOR, Const.CAN.REAR_RIGHT_ENCODER, Const.Angle.REAR_RIGHT_OFFSET));
+        }).build((swerve) -> {
+          // swerve.setFieldCentric(false);
+          // swerve.setIdleAngle(0, false);
 
-        //swerve.setIdleAngle(0, false);
+          swerve.setRampRate(Const.PID.SWERVE_MODULE_RAMP_RATE);
 
-        swerve.startLogging(new DashboardSwerveLogger());
-      });
+          swerve.forEachModule((location, module) -> {
+            PIDController modulePid = module.getPivotPIDController();
+            // modulePid.setOutputRampRate(Const.PID.SWERVE_MODULE_RAMP_RATE);
+            modulePid.setP(Const.PID.SWERVE_MODULE_P);
+            modulePid.setI(Const.PID.SWERVE_MODULE_I);
+            modulePid.setD(Const.PID.SWERVE_MODULE_D);
+          });
+
+          swerve.getLogger().outputTo(new DashboardLog());
+        });
   }
 }
